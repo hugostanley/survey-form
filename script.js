8a5d9efb78561317abeab72ce2c594187208301a
hugostanley <stanleyhugo06@gmail.com>
2022-05-04 20:33:40 +0800

    Create a response page

---
+++ script.js
@@ -0,0 +1,11 @@
+;(() => {
+    const searchParams = new URLSearchParams(window.location.search)
+    const name = searchParams.get('fullName')
+    console.log(name)
+    const domName = document.querySelector('[data-name]')
+    console.log(domName)
+  
+    if(domName) {
+      domName.textContent = 'Thank you ' + name 
+    }
+  })()
